fabd8f4f46a24ec293b717b6edcd221b4a90ded4
MatCer <cernymatej2000@gmail.com>
2024-08-28 16:52:49 +0200

    setup vue3 and inertiajs

---
+++ resources/js/Pages/Books/Index.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    props: {
+        books: Array,
+    },
+};
+</script>
+
+<template>
+    <h1>Books list</h1>
+    <ul>
+        <li v-for="book in books" :key="book.id">
+            {{ book.title }}
+        </li>
+    </ul>
+</template>
