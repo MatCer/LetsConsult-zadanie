b3d6e41ca470050ef9d78fef0be1221aff7c7431
MatCer <cernymatej2000@gmail.com>
2024-08-28 17:43:55 +0200

    add flash success alert

---
+++ resources/js/components/AlertSuccess.vue
@@ -0,0 +1,15 @@
+<script>
+export default {}
+</script>
+
+<template>
+    <div
+        v-if="$page.props.flash.success"
+        class="alert alert-success alert-dismissible fade show my-2"
+        role="alert"
+    >
+        {{ $page.props.flash.success }}
+
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+</template>
--- resources/js/components/Layout.vue
+++ resources/js/components/Layout.vue
@@ -1,12 +1,16 @@
 <script>
+import AlertSuccess from "@/components/AlertSuccess.vue";
+
 export default {
-    //
+    components: {AlertSuccess}
 }
 </script>
 
 <template>
     <main>
         <div class="container">
+            <AlertSuccess/>
+
             <h1 v-if="this.$slots.heading">
                 <slot name="heading"/>
             </h1>
